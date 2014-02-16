74347df76945ac8bd8d4a278567182fed2883650
emoore24 <evan.moore24@gmail.com>
2014-02-16 08:32:53 -0500

    changes #hackathon

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var exphbs = require('express3-handlebars');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
+var db= require('./db');
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        if (username !== "kcl-admin") {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+    }
+));
 
 var app = express();
 
@@ -24,6 +39,9 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // development only
 if ('development' == app.get('env')) {
@@ -35,6 +53,10 @@ app.get('/', routes.index);
 app.get('/browse', routes.browse);
 
 app.get('/quiz', routes.quiz);
+
+app.get('/admin', routes.admin);
+
+app.get('/browse/:category', routes.browseCat);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
