7bb6de21a43705671054b89c01646ca371c0b8e9
Tara Krishnan <tara.krish16@gmail.com>
2014-02-16 07:10:16 -0500

    quiz page added

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/', routes.index);
 
 app.get('/browse', routes.browse);
 
+app.get('/quiz', routes.quiz);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
