df58e27d113c6fb4319f855c958de0cf7409aa38
Tara Krishnan <tara.krish16@gmail.com>
2014-02-16 12:32:57 -0500

    working on words pages

--- public/javascripts/words.js
+++ public/javascripts/words.js
@@ -3,12 +3,25 @@ $(function(){
 	var curr_word ="Absorption";
 	$(".alph a").click(function(e){
 		e.preventDefault();
-		var l = $(this).data("letter");
+		var letter = $(this).data("letter");
+		var c = $(this).data("category").toLowerCase();
 		$(".alph a[data-letter="+curr_l+"]").removeClass("pure-menu-selected");
 		$(this).addClass("pure-menu-selected");
-		$(".list ul[data-letter="+l+"]").show();
+		$(".list ul[data-letter="+letter+"]").show();
 		$(".list ul[data-letter="+curr_l+"]").hide();
-		$.get('/letter_vids', {letter: l.toLowerCase()})
+		car l = letter.toLowerCase();
+		$.get('/letterVids', {letter: l, category:c},
+			function(data) {
+				if (data.success === "error"){
+					console.log("ERROR");
+				} else {
+					var source = "{{#each vids}} <div id='{{word}}' class='word-vid'><div class='word-title'>{{word}}</div><div class='word-video'><video src='{{url}}' controls></video></div></div>{{/each}}";
+					var template = Handlebars.compile(source);
+					var context = data;
+					var html = template(context);
+					$('.videos').html(html);
+				}
+			}, "json");
 	});
 
 	$(".word a").click(function(e){
